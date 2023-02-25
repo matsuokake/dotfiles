7c0110b6de6e5c9982fb491dfc4f5eda40dc7d4a
松岡健太朗 <matsuoka@matsuokakentarounoMacBook-Air.local>
2023-02-25 22:24:54 +0900

    [Add] initial_setting.sh

---
+++ vscode/initial_setting.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#mac+vscode+vim設定でのカーソル移動・連打問題の解決
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
